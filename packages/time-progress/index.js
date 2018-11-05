ba7496cf8a5fcf80f49055a4b871ed0a29f56abf
Alberto Parziale <alberto.parziale@caffeina.com>
2018-11-05 16:39:44 +0100

    update timeprogress logic

--- packages/time-progress/index.js
+++ packages/time-progress/index.js
@@ -1,26 +1,34 @@
 import {cap} from '@okiba/math'
 
 export default class TimeProgress {
-
   constructor(duration = 400) {
     this.duration = duration
+    this.direction = 1
+    this.elapsed = 0
+  }
+
+  updateDelta() {
+    this.time = Date.now()
+    if (!this.lastTime) {
+      this.lastTime = this.time
+    }
+    this.delta = this.time - this.lastTime
+    this.lastTime = this.time
+  }
+
+  setDirection(isForward) {
+    this.direction = isForward ? 1 : -1
+  }
+
+  setProgress(progress) {
+    this.progress = progress
+    this.elapsed = this.duration * this.progress
   }
 
   update() {
-    this.time = Date.now()
-    if (!this.startTime) {
-      this.startTime = this.lastTime = this.time
-    }
-    this.delta = this.time - this.lastTime
-    this.elapsed = this.time - this.startTime
+    this.updateDelta()
+    this.elapsed += this.direction * this.delta
     this.progress = cap(this.elapsed / this.duration, 0, 1)
-    this.lastTime = this.time
     return this.progress
-  }
-
-  reset() {
-    this.time = this.lastTime = this.startTime = null
-    this.progress = this.elapsed = null
-    this.delta = null
   }
 }
