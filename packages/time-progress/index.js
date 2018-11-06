76827c489a07d54ee54b2ba77ba2c02be28e7dc9
Alberto Parziale <alberto.parziale@caffeina.com>
2018-11-06 10:30:40 +0100

    [+] Refactor for private method, added active properties, round progress

--- packages/time-progress/index.js
+++ packages/time-progress/index.js
@@ -1,4 +1,23 @@
-import {cap} from '@okiba/math'
+import {cap, round} from '@okiba/math'
+
+function checkActive() {
+  if (this.direction === -1 && this.progress > 0) {
+    this.isActive = true
+  } else if (this.direction === 1 && this.progress < 1) {
+    this.isActive = true
+  } else {
+    this.isActive = false
+  }
+}
+
+function updateDelta() {
+  this.time = performance.now()
+  if (!this.lastTime) {
+    this.lastTime = this.time
+  }
+  this.delta = this.time - this.lastTime
+  this.lastTime = this.time
+}
 
 export default class TimeProgress {
   constructor(duration = 400) {
@@ -12,28 +31,21 @@ export default class TimeProgress {
   }
 
   setProgress(progress) {
-    this.elapsed = this.duration * this.progress
+    this.elapsed = this.duration * Math.abs(this.progress - progress)
     this.progress = progress
   }
 
   update() {
-    this._updateDelta()
+    updateDelta.call(this)
     this.elapsed += this.direction * this.delta
-    this.progress = cap(this.elapsed / this.duration, 0, 1)
+    this.progress = this.elapsed / this.duration
+    checkActive.call(this)
+    this.progress = round(cap(this.elapsed / this.duration, 0, 1), 3)
     return this.progress
   }
 
   reset() {
     this.time = this.lastTime = this.delta = null
     this.progress = this.elapsed = 0
-  }
-
-  _updateDelta() {
-    this.time = Date.now()
-    if (!this.lastTime) {
-      this.lastTime = this.time
-    }
-    this.delta = this.time - this.lastTime
-    this.lastTime = this.time
   }
 }
