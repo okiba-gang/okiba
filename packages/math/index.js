a912d7ea36b5f518d2b14eac348c9d5ce88a1254
Alberto Parziale <alberto.parziale@caffeina.com>
2018-11-06 10:29:15 +0100

    [+] Round function

--- packages/math/index.js
+++ packages/math/index.js
@@ -9,3 +9,11 @@ export function cap(n, min, max) {
 export function distance(x1, x2) {
   return Math.abs(x1 - x2)
 }
+
+const roundMap = {}
+export function round(n, p = 3) {
+  if (!roundMap[p]) {
+    roundMap[p] = Math.pow(10, p)
+  }
+  return Math.round(n * roundMap[p]) / roundMap[p]
+}
