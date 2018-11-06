b85c54e5c717654c9d4450e9e8de89f35a7e2e61
Vincent De Feo <4696598+vinzdef@users.noreply.github.com>
2018-11-06 18:24:55 +0100

    [+][arrays] Docs

--- packages/arrays/index.js
+++ packages/arrays/index.js
@@ -1,3 +1,22 @@
+/**
+ * @module Okiba / arrays
+ * @description Utilities to work with arrays
+ */
+
+/**
+ * Return the first element if it only contains one
+ *
+ * ```javascript
+ * const els = arrayOrOne([🍏, 🍌])
+ * console.log(els) // [🍏, 🍌]
+ *
+ * const els = arrayOrOne([🍏])
+ * console.log(els) // 🍏
+ * ```
+ *
+ * @param {Array-like} arrayLike The options object.
+ * @returns {any} The first element or the argument
+ */
 export function arrayOrOne(arrayLike) {
   if (arrayLike.length === 1) {
     return arrayLike[0]
@@ -6,6 +25,12 @@ export function arrayOrOne(arrayLike) {
   return arrayLike
 }
 
+/**
+ * Cast an array-like object or single element to Array
+ *
+ * @param {any} castable Array to cast
+ * @returns {Array} The array-like converted to Array, or an Array containing the element
+ */
 export function castArray(castable) {
   if (castable instanceof Array) {
     return castable
