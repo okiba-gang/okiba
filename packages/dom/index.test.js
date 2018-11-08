badf36ba237fc3f117ee1cb6a15ca838067598f5
Vincent De Feo <vincent.defeo@caffeina.com>
2018-11-08 15:43:59 +0100

    [X][dom] fix tests

--- packages/dom/index.test.js
+++ packages/dom/index.test.js
@@ -1,17 +1,24 @@
 import {qsa} from './'
+
 import { JSDOM } from 'jsdom'
+const { document } = (new JSDOM(`<div class="component">
+    <div class="element"></div>
+    <div class="element"></div>
+    <div class="element"></div>
+  </div>`, {url: 'https://example.org/'})).window
+global.document = document
 
-const { document } = (new JSDOM(`
-    <div class="el"></div>
-    <div class="el"></div>
-  `)).window
-
-const DOMNodes = document.querySelectorAll('.el')
-
-test('qsa should return an array with the correct elements', done => {
-  const nodes = qsa('.el', document)
+test('qsa should return an array with the correct elements and default to document', done => {
+  const nodes = qsa('.element')
   expect(nodes instanceof Array).toBe(true)
-  expect(nodes).toEqual(DOMNodes)
+  expect(nodes).toEqual(Array.from(document.querySelectorAll('.element')))
+  done()
+})
+
+test('qsa should return an array with the correct elements, scoped to an element', done => {
+  const nodes = qsa('.element', document.querySelector('.component'))
+  expect(nodes instanceof Array).toBe(true)
+  expect(nodes).toEqual(Array.from(document.querySelectorAll('.element')))
   done()
 })
 
