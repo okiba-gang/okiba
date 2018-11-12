95088fc344ba9663ad143a5640a43bf1f676625b
Vincent De Feo <4696598+vinzdef@users.noreply.github.com>
2018-11-12 15:02:28 +0100

    Update test messages and test for closest target (#27)
    * Update packages/search/index.test.js
    
    [+] [search] test
    
    * [+][search] fix test
    
    * [+][search] Update test messages and test for closest target
    
    * Update packages/search/index.test.js
    
    [+] [search] test

--- packages/search/index.test.js
+++ packages/search/index.test.js
@@ -1,31 +1,36 @@
 import {binarySearch} from './'
 
-const length = 50;
-const data = [];
+const length = 50
+const data = []
 for (let i = 0; i < length; i++) {
-  data.push(i);
+  data.push(i)
 }
 
-const dataObj = [];
+const dataObj = []
 for (let i = 0; i < length; i++) {
-  dataObj.push({i});
+  dataObj.push({i})
 }
 
-test('find 5 in array', done => {
-  const f = binarySearch(data, 5, 0, data.length-1)
+test('should find an element in an array', done => {
+  const f = binarySearch(data, 5, 0, data.length - 1)
   expect(f).toBe(5)
   done()
 })
 
-
-test('find prop i with value 5 in array of object', done => {
-  const f = binarySearch(dataObj, 5, 0, dataObj.length-1, 'i')
+test('should find object with exact prop in an array of object', done => {
+  const f = binarySearch(dataObj, 5, 0, dataObj.length - 1, 'i')
   expect(f).toBe(5)
   done()
 })
 
-test('find prop i with value 0 in array of object', done => {
-  const f = binarySearch(dataObj, 0, 0, dataObj.length-1, 'i')
+test('should find the closest element to the left', done => {
+  const f = binarySearch(data, 3.3, 0, data.length - 1)
+  expect(f).toBe(3)
+  done()
+})
+
+test('should find an element at the start of the array', done => {
+  const f = binarySearch(dataObj, 0, 0, dataObj.length - 1, 'i')
   expect(f).toBe(0)
   done()
 })
