57f3b6a8151e9dd1ab3701e5320cadacf1619a66
Vincent De Feo <vincent.defeo@caffeina.com>
2018-11-14 11:17:39 +0100

    [test][evented-component] 100% tested

--- packages/event-emitter/index.test.js
+++ packages/event-emitter/index.test.js
@@ -20,3 +20,24 @@ it('should remove an handler and not call it', done => {
   expect(spy).not.toBeCalled()
   done()
 })
+
+it('should not remove an handler if it has none', done => {
+  const ee = new EventEmitter()
+
+  const hs = Object.assign({}, ee.hs)
+  ee.off('test-event', _ => _)
+
+  expect(hs).toEqual(ee.hs)
+  done()
+})
+
+it('should not remove an handler if reference is wrong', done => {
+  const ee = new EventEmitter()
+
+  ee.on('test-event', function test() {})
+  const hs = Object.assign({}, ee.hs)
+  ee.off('test-event', _ => _)
+
+  expect(hs).toEqual(ee.hs)
+  done()
+})
