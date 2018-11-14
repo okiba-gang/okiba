f7d98d8ff4bc360216535f10e092c59f045129cf
lavolpecheprogramma <parziale.alberto@gmail.com>
2018-11-14 09:52:18 +0100

    [+][dom] event coords & test (#31)
    * [+][dom] event coords method
    
    * [+][dom] event coords method
    
    * [+][dom] add test eventCoord and fix coods name
    
    * [+] update docs

--- packages/dom/index.js
+++ packages/dom/index.js
@@ -67,11 +67,11 @@ function evt(source, type, handler, action) {
  * // adds `onClick` to 'click' and `onMouseChange` to both 'mouseenter' and 'mouseleave'
  * on(buttons, ['click', mouseenter', 'mouseleve'], [onClick, onMouseChange])
  *
- * @param {Element|[]Element} [window] source
+ * @param {(Element|Element[])} [window] source
  * the element which will trigger the event
- * @param {String|[]String} type
+ * @param {(String|String[])} type
  * the event name to bind. Or an array of
- * @param {Function|[]Function} handler
+ * @param {(Function|Function[])} handler
  * the callback to be fired at the event. If an array is supplied the handlers will be bound in order,
  * if there are less handlers than event types, the last handler is bound to all remaining events.
  *
@@ -99,11 +99,11 @@ export function on(source, type, handler) {
  * // removes `onClick` from 'click' and `onMouseChange` from both 'mouseenter' and 'mouseleave'
  * off(buttons, ['click', mouseenter', 'mouseleve'], [onClick, onMouseChange])
  *
- * @param {Element|[]Element} [window] source
+ * @param {(Element|Element[])} [window] source
  * Element which will trigger the event
- * @param {String|[]String} type
+ * @param {(String|String[])} type
  * Event name to unbind. Or an array of
- * @param {Function|[]Function} handler
+ * @param {(Function|Function[])} handler
  * Callback bound to the event. If an array is supplied the handlers will be unbound in order,
  * if there are less handlers than event types, the last handler is unbound from all remaining events.
  *
@@ -111,4 +111,32 @@ export function on(source, type, handler) {
  */
 export function off(source, type, handler) {
   return evt(source, type, handler, 'remove')
+}
+
+/**
+ *
+ * Read mouse and touch position in the same way
+ *
+ * @example
+ * import {eventCoords, on} from '@okiba/dom'
+ * on(window, ['mousemove', 'touchmove'], onMove)
+ *
+ * function onMove(e){
+ *  const coords = eventCoords(e)
+ *  console.log(coords)
+ * }
+ *
+ * @param {Event} DOM Event
+ *
+ * @return {Object} Event position coordinates (clientX and ClientY)
+ */
+export function eventCoords(event) {
+  let coords = event
+  if (event.type.indexOf('touch') === 0) {
+    coords = event.touches[0] || event.changedTouches[0]
+  }
+  return {
+    clientX: coords.clientX,
+    clientY: coords.clientY,
+  }
 }
